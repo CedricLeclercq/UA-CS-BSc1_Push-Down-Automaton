a3332c790b8e40c1b8c0099fd66addbc951b692d
cedricleclercq <cedric.leclercq2002@gmail.com>
2021-11-08 14:35:13 +0100

    seems to fully work

--- CFG.cpp
+++ CFG.cpp
@@ -127,6 +127,10 @@ bool CFG::accepts(const string& str) {
         for (int j = 0; j < (int)test.size(); j++) {
             string toTest = test[j]; // what we should use for calculating
 
+            if (i == 3 and j == 0) {
+                cout << "";
+            }
+
             // base case, we are on the first row
             if (i == 1) {
                 this->table[i-1][j] = this->findCharInProd(toTest);
@@ -134,7 +138,7 @@ bool CFG::accepts(const string& str) {
             }
 
             // Normal case (where we are NOT at the first row)
-            else {
+            else if (toTest.size() == 2){
                 // TODO if the input is longer than 2 we should divide the input into the prev. findings - FIRST SEE IF THIS IS REALLY NEEDED
                 // First calculating cartesian product of what we need
                 vector<vector<string>> allComb; // TODO check if this is even correct
@@ -163,7 +167,60 @@ bool CFG::accepts(const string& str) {
 
                 // We found all the matches, we can store it into the table
                 this->table[i-1][j] = matches;
-                this->printTable();
+                allResults.emplace_back(toTest,this->table[i-1][j]);
+                //this->printTable();
+
+
+
+            } else {
+                // First splitting input, we just need to move the comma
+                for (int it = 1; it < toTest.size(); it++) {
+                    string sub1 = toTest.substr(0,it);
+                    string sub2 = toTest.substr(it,string::npos);
+                    cout << toTest << " " << sub1  << " " << sub2 << endl; // TODO remove debug code
+
+
+                    // The input is now split, we can search for its substrings in our allResults vector
+                    pair<vector<string>,vector<string>> found;
+                    for (const auto& result: allResults) {
+                        if (result.first == sub1) {
+                            found.first = result.second;
+                        }
+                        if (result.first == sub2) {
+                            found.second = result.second;
+                        }
+                    }
+
+                    // We have the two vectors that we need to calculate the cartesian product of
+                    vector<vector<string>> cartProd = cartesianProduct({found.first,found.second});
+
+                    vector<string> matches;
+                    for (const auto& item: cartProd) {
+                        string str1;
+                        for (const auto& item1: item)
+                            str1 += item1;
+                        for (const auto& prod: this->productions) {
+                            string str2;
+                            for (const auto& item2: prod.second) {
+                                str2 += item2;
+                            }
+                            if (str1 == str2) {
+                                matches.emplace_back(prod.first);
+                            }
+                        }
+                    }
+
+                    // We found all the matches, we can store it into the table
+                    for (const auto& id: matches) {
+                        if (find(this->table[i-1][j].begin(), this->table[i-1][j].end(), id) == this->table[i-1][j].end()) {
+                            this->table[i - 1][j].push_back(id);
+                        }
+
+                    }
+                    //this->printTable();
+                }
+                allResults.emplace_back(toTest,this->table[i-1][j]);
+                sort(this->table[i-1][j].begin(),this->table[i-1][j].end(), compareWords);
             }
         }
     }
@@ -188,6 +245,21 @@ vector<string> CFG::splitInPieces(const string& str, int amount) {
 void CFG::printTable() {
     for (int i = (int)this->table.size() - 1; i >= 0; i--) {
         cout << "|";
+        int k = 0;
+        for (auto it2 = this->table[i].begin(); it2 != this->table[i].end(); it2++) {
+            k++;
+            string s = " {";
+            for (auto it = (*it2).begin(); it != (*it2).end(); it++) {
+                s+= (*it);
+                if (next(it) != (*it2).end()) {
+                    s += ", ";
+                } else cout << s + "}  |";
+            }
+            if (k < this->table.size() - i and (*it2).empty())
+                cout << s << "}  |";
+
+        } cout << endl;
+        /*
         for (auto & j : this->table[i]) { // Or j == size() see for later
             string s = " {";
             for (auto it = j.begin(); it != j.end(); it++) {
@@ -196,7 +268,10 @@ void CFG::printTable() {
                     s += ", ";
                 } else cout << s + "}  |";
             }
+            if (j.empty())
+                cout << s << "}  |";
         } cout << endl;
+         */
     }
 }
 
